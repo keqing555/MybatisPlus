0b4ef2eae47cae24dcf083c0c5738877675172cd
keqing555 <1845441591@qq.com>
2021-10-21 09:16:07 +0800

    Mybatisplus基本查询

---
+++ src/main/java/com/mybatisplus/dao/RoleMapper.java
@@ -0,0 +1,7 @@
+package com.mybatisplus.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mybatisplus.bean.Role;
+
+public interface RoleMapper extends BaseMapper<Role> {
+}
--- src/test/java/com/mybatisplus/MybatisPlusApplicationTests.java
+++ src/test/java/com/mybatisplus/MybatisPlusApplicationTests.java
@@ -1,13 +1,24 @@
 package com.mybatisplus;
 
+import com.mybatisplus.bean.Role;
+import com.mybatisplus.dao.RoleMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
+
 class MybatisPlusApplicationTests {
+    @Autowired
+    private RoleMapper roleMapper;
 
     @Test
     void contextLoads() {
+        //查询所有
+        List<Role> role = roleMapper.selectList(null);
+        role.forEach(System.out::println);
     }
 
 }
